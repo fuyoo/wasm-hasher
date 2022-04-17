8b0cf693c7c85bb49b7b263b08b1e297b52f4127
fuyoo <fyaiym@qq.com>
2022-04-17 15:52:52 +0800

    update to bundler

--- pkg/wasm_hasher.d.ts
+++ pkg/wasm_hasher.d.ts
@@ -66,36 +66,3 @@ export function sha3_512(blob: Blob, cb: Function): Promise<string>;
 * @returns {Promise<string>}
 */
 export function sm3(blob: Blob, cb: Function): Promise<string>;
-
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
-
-export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly md5: (a: number, b: number) => number;
-  readonly sha2_224: (a: number, b: number) => number;
-  readonly sha2_256: (a: number, b: number) => number;
-  readonly sha2_384: (a: number, b: number) => number;
-  readonly sha2_512: (a: number, b: number) => number;
-  readonly sha1: (a: number, b: number) => number;
-  readonly sha3_224: (a: number, b: number) => number;
-  readonly sha3_256: (a: number, b: number) => number;
-  readonly sha3_384: (a: number, b: number) => number;
-  readonly sha3_512: (a: number, b: number) => number;
-  readonly sm3: (a: number, b: number) => number;
-  readonly __wbindgen_malloc: (a: number) => number;
-  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
-  readonly __wbindgen_export_2: WebAssembly.Table;
-  readonly _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hbac97d8a46b64b05: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_exn_store: (a: number) => void;
-  readonly wasm_bindgen__convert__closures__invoke2_mut__h0ad39b4c1c0bacfb: (a: number, b: number, c: number, d: number) => void;
-}
-
-/**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {InitInput | Promise<InitInput>} module_or_path
-*
-* @returns {Promise<InitOutput>}
-*/
-export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
